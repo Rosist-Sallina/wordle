6c3fddfd09fcf603e75ce4bca037c444f2556689
Rosist <umaumaliu@outlook.com>
2024-07-01 10:14:47 +0800

    init

---
+++ wordle/target/debug/deps/wordle-f61bc5db3605554d.d
@@ -0,0 +1,5 @@
+/rust/wordle/wordle/wordle/target/debug/deps/libwordle-f61bc5db3605554d.rmeta: src/main.rs
+
+/rust/wordle/wordle/wordle/target/debug/deps/wordle-f61bc5db3605554d.d: src/main.rs
+
+src/main.rs:
