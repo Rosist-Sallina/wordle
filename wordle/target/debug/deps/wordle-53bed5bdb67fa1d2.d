76230fd72a3f25b0723413b0e73081dd2e843a6c
Rosist <umaumaliu@outlook.com>
2024-07-01 10:28:13 +0800

    Delete unused files

--- wordle/target/debug/deps/wordle-53bed5bdb67fa1d2.d
+++
@@ -1,5 +0,0 @@
-/rust/wordle/wordle/wordle/target/debug/deps/libwordle-53bed5bdb67fa1d2.rmeta: src/main.rs
-
-/rust/wordle/wordle/wordle/target/debug/deps/wordle-53bed5bdb67fa1d2.d: src/main.rs
-
-src/main.rs:
