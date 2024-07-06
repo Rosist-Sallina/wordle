ab782f0975c912512308cd5c118d1982083c713a
Rosist <umaumaliu@outlook.com>
2024-07-06 21:05:25 +0800

    Feat : test if derive can work

--- src/main.rs
+++ src/main.rs
@@ -4,92 +4,95 @@ use std::io::{self, BufRead, BufReader, Write};
 use std::collections::HashMap;
 use clap::{Arg, command, value_parser , ArgAction};
 use std::collections::HashSet;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let is_tty = atty::is(atty::Stream::Stdout);
+use clap::Parser;
 
-    let mut count_success = 0;
-    let mut count_played = 0;
-    let mut count_success_loop = 0;
+#[derive(Parser, Debug)]
+#[command(name = "wordle", version = "0.1.0", about = "a simple wordle game")]
+struct Args {
+    /// Sets the word to guess
+    #[arg(short, long, value_name = "WORD")]
+    word: Option<String>,
 
-    if is_tty {
-        Ok(())
-    } else {
-        // let mut success = 0;
-        let mut w_mode = false;
-        let mut answer = String::new();
-        let matches = command!() // requires `cargo` feature
-        .version("0.1.0")
-        .about("a simple wordle game")
-        .arg(Arg::new("word")
-            .short('w')
-            .long("word")
-            .value_name("WORD")
-            .help("Sets the word to guess")
-            .required(false)
-            .num_args(0..=1)
-            .value_parser(value_parser!(String)))
-        .arg(Arg::new("random")
-            .short('r')
-            .long("random")
-            .help("random mode")
-            .required(false)
-            .action(ArgAction::SetTrue))
-        .arg(Arg::new("difficult")
-            .short('D')
-            .long("difficult")
-            .help("start difficult mode")
-            .required(false)
-            .action(ArgAction::SetTrue))
-        .arg(Arg::new("stats")
-            .short('t')
-            .long("stats")
-            .help("print the state of the game")
-            .action(ArgAction::SetTrue)
-            .required(false))
-        .arg(Arg::new("day")
-            .short('d')
-            .long("day")
-            .value_name("DAY")
-            .help("how many rounds you want to loop")
-            .required(false)
-            .value_parser(value_parser!(usize)))
-        .arg(Arg::new("seed")
-            .short('s')
-            .long("seed")
-            .value_name("SEED")
-            .help("seed for random")
-            .required(false)
-            .value_parser(value_parser!(i32)))
-        .arg(Arg::new("final-set")
-            .short('f')
-            .long("final-set")
-            .value_name("FINAL_SET")
-            .help("final set of words")
-            .required(false)
-            .value_parser(value_parser!(String)))
-        .arg(Arg::new("acceptable-set")
-            .short('a')
-            .long("acceptable-set")
-            .value_name("ACCEPTABLE_SET")
-            .help("acceptable set of words")
-            .required(false)
-            .value_parser(value_parser!(String)))
-        .arg(Arg::new("state")
-            .short('S')
-            .long("state")
-            .value_name("STATE")
-            .help("make the result into a json")
-            .required(false)
-            .value_parser(value_parser!(String)))
-        .arg(Arg::new("config")
-            .short('c')
-            .long("config")
-            .value_name("CONFIG")
-            .help("config file")
-            .required(false)
-            .value_parser(value_parser!(String)))
-        .get_matches();        
-        Ok(())
-    }
+    /// Random mode
+    #[arg(short, long)]
+    random: bool,
+
+    /// Start difficult mode
+    #[arg(short = 'D', long)]
+    difficult: bool,
+
+    /// Print the state of the game
+    #[arg(short, long)]
+    stats: bool,
+
+    /// How many rounds you want to loop
+    #[arg(short, long, value_name = "DAY")]
+    day: Option<usize>,
+
+    /// Seed for random
+    #[arg(short, long, value_name = "SEED")]
+    seed: Option<i32>,
+
+    /// Final set of words
+    #[arg(short = 'f', long, value_name = "FINAL_SET")]
+    final_set: Option<String>,
+
+    /// Acceptable set of words
+    #[arg(short, long, value_name = "ACCEPTABLE_SET")]
+    acceptable_set: Option<String>,
+
+    /// Make the result into a json
+    #[arg(short = 'S', long, value_name = "STATE")]
+    state: Option<String>,
+
+    /// Config file
+    #[arg(short, long, value_name = "CONFIG")]
+    config: Option<String>,
 }
 
+fn main() {
+    let args = Args::parse();
+
+    // 处理解析后的参数
+    if let Some(word) = args.word {
+        println!("Word to guess: {}", word);
+    }
+
+    if args.random {
+        println!("Random mode is on");
+    }
+
+    if args.difficult {
+        println!("Difficult mode is on");
+    }
+
+    if args.stats {
+        println!("Printing game stats");
+    }
+
+    if let Some(day) = args.day {
+        println!("Number of rounds to loop: {}", day);
+    }
+
+    if let Some(seed) = args.seed {
+        println!("Seed for random: {}", seed);
+    }
+
+    if let Some(final_set) = args.final_set {
+        println!("Final set of words: {}", final_set);
+    }
+
+    if let Some(acceptable_set) = args.acceptable_set {
+        println!("Acceptable set of words: {}", acceptable_set);
+    }
+
+    if let Some(state) = args.state {
+        println!("State file: {}", state);
+    }
+
+    if let Some(config) = args.config {
+        println!("Config file: {}", config);
+    }
+
+    // 其他逻辑处理...
+}
